3cb070bdd96fa739bdc8155a19f5a0371ceebc8e
trong-nguyen <nguyentrong@gmail.com>
2017-12-03 13:51:56 -0500

    Added github contribution calendar

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// trongn's javascript
+
+(function ($) {
+    'use strict';
+
+    $(document).on('ready', function () {
+        GitHubCalendar('.calendar', 'trong-nguyen');
+    });
+
+})(jQuery);
